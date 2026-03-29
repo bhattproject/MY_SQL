031079ca51a8d1033b657d08e7b1a61838989c6f
bhattproject <bhattjirajeev@gmail.com>
2026-03-29 23:42:24 +0530

    Add SQL query to calculate median search count

--- 3.sql
+++ 3.sql
@@ -10,3 +10,20 @@ searches(search_count INT, num_users INT)
 +---------------+-----------+
 
 '''
+WITH expanded AS (
+    SELECT 
+        search_count,
+        num_users,
+        SUM(num_users) OVER (ORDER BY search_count) AS cum_users,
+        SUM(num_users) OVER () AS total_users
+    FROM searches
+),
+median_pos AS (
+    SELECT *,
+           (total_users + 1) / 2 AS m1,
+           (total_users + 2) / 2 AS m2
+    FROM expanded
+)
+SELECT AVG(search_count) AS median
+FROM median_pos
+WHERE cum_users >= m1 AND cum_users - num_users < m2;

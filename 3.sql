ad09fab07c694ac0798e13f6617002694af85257
bhattproject <bhattjirajeev@gmail.com>
2026-03-29 23:42:06 +0530

    Create 3.sql

---
+++ 3.sql
@@ -0,0 +1,12 @@
+'''Median from Frequency Table
+searches(search_count INT, num_users INT)
+
++---------------+-----------+
+| search_count  | num_users |
++---------------+-----------+
+|      1        |     2     |
+|      2        |     3     |
+|      3        |     1     |
++---------------+-----------+
+
+'''

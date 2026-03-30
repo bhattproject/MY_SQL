f9d273ede850470fa75da491cfc9f7fd94b93eca
bhattproject <bhattjirajeev@gmail.com>
2026-03-31 01:01:05 +0530

    Create 5.sql

---
+++ 5.sql
@@ -0,0 +1,14 @@
+'''
+
+RIGHT JOIN Orders
+👉 Keep all rows from Orders (right table)
+👉 If no match on left → fill LEFT side with NULL
+
+
+
+LEFT JOIN Orders
+👉 Keep all rows from LEFT side (the result built so far)
+👉 If no match in Orders → fill RIGHT side (o.*) with NULL
+
+  
+'''

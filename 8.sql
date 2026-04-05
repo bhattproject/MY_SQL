030577811f8df2c804ffda853228cf30b7e1f8fc
bhattproject <bhattjirajeev@gmail.com>
2026-04-05 23:45:26 +0530

    Update 8.sql

--- 8.sql
+++ 8.sql
@@ -55,3 +55,18 @@ median
 2.5
 
 
+Solution:
+WITH searches_expanded AS (
+  SELECT searches
+  FROM search_frequency
+  GROUP BY 
+    searches, 
+    GENERATE_SERIES(1, num_users))
+
+SELECT 
+  ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (
+    ORDER BY searches)::DECIMAL, 1) AS  median
+FROM searches_expanded;
+
+
+

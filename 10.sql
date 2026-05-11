1eddce3521f61fbfd7f84425e472c6e524d0b3a0
bhattproject <bhattjirajeev@gmail.com>
2026-05-12 00:00:15 +0530

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -1,1 +1,5 @@
 The most common way to fetch all data from a table (e.g., a "Customers" table) is:sqlSELECT * FROM Customers;
+Problem: Find all employees who earn more than their direct manager.Concept: Use a Self-Join to link the table to itself.sqlSELECT e1.name AS employee_name
+FROM Employees e1
+JOIN Employees e2 ON e1.manager_id = e2.employee_id
+WHERE e1.salary > e2.salary;

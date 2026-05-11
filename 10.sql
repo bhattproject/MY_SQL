117e072552d35824620ebaa72f8cf3a15ca1523f
bhattproject <bhattjirajeev@gmail.com>
2026-05-12 00:00:38 +0530

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -3,3 +3,8 @@ Problem: Find all employees who earn more than their direct manager.Concept: Use
 FROM Employees e1
 JOIN Employees e2 ON e1.manager_id = e2.employee_id
 WHERE e1.salary > e2.salary;
+
+
+. The Nth Highest SalaryProblem: Find the 2nd highest salary in a table without using LIMIT or TOP.Concept: Use a Correlated Subquery to find the maximum salary that is not the absolute maximum.sqlSELECT MAX(salary) 
+FROM Employees 
+WHERE salary NOT IN (SELECT MAX(salary) FROM Employees);

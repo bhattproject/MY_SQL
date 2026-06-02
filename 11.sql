cc089f1a6ecfb6f615e530ca706f5e20d9f7a2a1
bhattproject <bhattjirajeev@gmail.com>
2026-06-02 23:50:02 +0530

    Update 11.sql

--- 11.sql
+++ 11.sql
@@ -20,4 +20,33 @@ WHERE salary < (
     SELECT MAX(salary)
     FROM Employee
 );
+Explanation
+Inner query finds the highest salary (7000).
+Outer query finds the maximum salary less than 7000.
+Result: 6000.
+Solution 2: Using DENSE_RANK() (Modern SQL)
+SELECT salary
+FROM (
+    SELECT salary,
+           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+    FROM Employee
+) t
+WHERE rnk = 2;
 
+
+'''
+Explanation
+
+Ranking:
+
+salary	rnk
+7000	1
+7000	1
+6000	2
+5000	3
+
+The query returns 6000.
+
+Follow-up Interview Question
+Find the Nth Highest Salary
+  '''

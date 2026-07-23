c27e3752e01da4203a59864aab372027447fcef2
bhattproject <bhattjirajeev@gmail.com>
2026-07-24 00:41:18 +0530

    Fix SQL query description formatting
    Corrected formatting of the SQL query description.

--- 13.sql
+++ 13.sql
@@ -1,5 +1,5 @@
-You have a table named transactions with columns source, target, and messages. Two users can message each other in any direction (e.g., User A to User B, or User B to User A). Write a SQL query to find the total number of messages exchanged between any unique pair of users, regardless of who initiated the message.
-
+'''You have a table named transactions with columns source, target, and messages. Two users can message each other in any direction (e.g., User A to User B, or User B to User A). Write a SQL query to find the total number of messages exchanged between any unique pair of users, regardless of who initiated the message.
+'''
 
   SELECT 
     user1, 

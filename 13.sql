5cc5829a1d833f3d89d4826303df850dda557431
bhattproject <bhattjirajeev@gmail.com>
2026-07-23 23:58:08 +0530

    Create 13.sql

---
+++ 13.sql
@@ -0,0 +1,2 @@
+You have a table named transactions with columns source, target, and messages. Two users can message each other in any direction (e.g., User A to User B, or User B to User A). Write a SQL query to find the total number of messages exchanged between any unique pair of users, regardless of who initiated the message.
+

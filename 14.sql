37ab722d0560e9cfb456acec9e8d702d1f190799
bhattproject <bhattjirajeev@gmail.com>
2026-07-31 00:00:10 +0530

    Add problem statement for fraud detection query
    Added a problem statement for identifying potential fraudulent payments using SQL.

--- 14.sql
+++ 14.sql
@@ -2,3 +2,4 @@ Question 1: Fraudulent Repeated Payments (Stripe)Concept: Window Functions, Time
   identify potential fraud or accidental double-clicks. Write a query to identify any payments made at the same merchant with the same credit card for the same amount within
   10 minutes of each other
 
+Schema (transactions)transaction_id (INT)merchant_id (INT)credit_card_id (INT)amount (DECIMAL)transaction_timestamp (TIMESTAMP)

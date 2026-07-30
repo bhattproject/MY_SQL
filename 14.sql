6cbc6d3e219662634a500c0db3543d3ff635b896
bhattproject <bhattjirajeev@gmail.com>
2026-07-30 23:59:34 +0530

    Create 14.sql

---
+++ 14.sql
@@ -0,0 +1,4 @@
+Question 1: Fraudulent Repeated Payments (Stripe)Concept: Window Functions, Time-interval filtering, and Self-joins.Problem StatementA online payment processor wants to
+  identify potential fraud or accidental double-clicks. Write a query to identify any payments made at the same merchant with the same credit card for the same amount within
+  10 minutes of each other
+

d8b11920b01a294276d24d5d4677b0726dde79c4
bhattproject <bhattjirajeev@gmail.com>
2026-08-25 23:59:52 +0530

    Create 16.sql

---
+++ 16.sql
@@ -0,0 +1,3 @@
+Problem 1: Gender-Wise Percentage DistributionIt checks your ability to
+  compute percentages and format strings properly.The Scenario:You are given an Employee table with columns id, name, and gender. Write a query to find the 
+  percentage distribution of each gender across the company. The output should:Show the percentage for each gender.Be rounded to 2 decimal places.Include the % symbol
